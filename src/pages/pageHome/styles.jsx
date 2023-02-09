6374623a6ce23411b3367bc3d7bcff71175674e4
Igor Gomes <iguinhogms@gmail.com>
2023-02-08 23:36:54 -0300

    inicio das alteracoes

--- src/pages/pageHome/styles.jsx
+++ src/pages/pageHome/styles.jsx
@@ -8,11 +8,3 @@ export const HomeContainer = styled.div`
   background-image: linear-gradient(black, black, #141414);
   z-index: -2;
 `;
-
-export const TypeAnimationContainer = styled.div`
-  position: relative;
-  height: 100vh;
-  width: 100vw;
-  margin-top: 10%;
-  padding: 10vh 15vw 0 15vw;
-`;
