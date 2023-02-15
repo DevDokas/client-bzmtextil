76902b53ad4ee376dd482522682289c30dcd5fec
Igor Gomes <iguinhogms@gmail.com>
2023-02-15 00:40:31 -0300

    pagina de direcionamento whatsapp

--- src/pages/pageWhatsapp/index.jsx
+++ src/pages/pageWhatsapp/index.jsx
@@ -1,23 +1,87 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Container, Logo, List, Item, Button } from './styles';
+import {
+  Container,
+  Logo,
+  List,
+  Item,
+  Link,
+  Title,
+  WhatsAppLogo,
+  ProfileImage,
+} from './styles';
 
 import bzmlogo from '../../assets/bzmlogo.png';
+import whatsapp from '../../assets/whatsapp.png';
+import woman1 from '../../assets/woman1.jpg';
+import woman2 from '../../assets/woman2.jpg';
+import woman3 from '../../assets/woman3.jpg';
 
 export default function WhatsApp() {
   const navigate = useNavigate();
 
   return (
     <Container>
-      <Logo src={bzmlogo} />
-      <p>Converse com nossas consultoras de vendas!</p>
+      <Logo src={bzmlogo} onClick={() => navigate(-1)} />
+      <Title>Contate-nos também pelo WhatsApp!</Title>
+      <WhatsAppLogo src={whatsapp} />
       <List>
-        <Item>1</Item>
-        <Item>2</Item>
-        <Item>3</Item>
+        <Item>
+          <ProfileImage
+            src={woman1}
+            onClick={() =>
+              window.open(
+                'https://api.whatsapp.com/message/VAPFNMXDPKKUK1?autoload=1&app_absent=0',
+                '_blank'
+              )
+            }
+          />
+          <Link
+            href="https://api.whatsapp.com/message/VAPFNMXDPKKUK1?autoload=1&app_absent=0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Jéssica
+          </Link>
+        </Item>
+        <Item>
+          <ProfileImage
+            src={woman2}
+            onClick={() =>
+              window.open(
+                'https://api.whatsapp.com/qr/34C3L2I6AYFUA1?autoload=1&app_absent=0',
+                '_blank'
+              )
+            }
+          />
+          <Link
+            href="https://api.whatsapp.com/qr/34C3L2I6AYFUA1?autoload=1&app_absent=0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dayani
+          </Link>
+        </Item>
+        <Item>
+          <ProfileImage
+            src={woman3}
+            onClick={() =>
+              window.open(
+                'https://api.whatsapp.com/qr/7ZWXTPBAKQJZO1?autoload=1&app_absent=0',
+                '_blank'
+              )
+            }
+          />
+          <Link
+            href="https://api.whatsapp.com/qr/7ZWXTPBAKQJZO1?autoload=1&app_absent=0"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Patrícia
+          </Link>
+        </Item>
       </List>
-      <Button onClick={() => navigate(-1)}>Voltar</Button>
     </Container>
   );
 }
--- src/pages/pageWhatsapp/styles.jsx
+++ src/pages/pageWhatsapp/styles.jsx
@@ -7,13 +7,53 @@ export const Container = styled.div`
 `;
 
 export const Logo = styled.img`
+  margin-top: 50px;
   height: 10vh;
+  cursor: pointer;
+`;
+
+export const WhatsAppLogo = styled.img`
+  height: 10vh;
+  margin-top: 25px;
 `;
 
 export const List = styled.ul`
+  margin-top: 30px;
   list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
 `;
 
-export const Item = styled.li``;
+export const Item = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: grey;
+  height: 120px;
+  width: fit-content;
+  padding: 15px;
+  border-radius: 10px;
+`;
 
 export const Button = styled.button``;
+
+export const Link = styled.a`
+  font-size: 24px;
+  text-decoration: none;
+  color: black;
+`;
+
+export const Title = styled.p`
+  font-size: 18px;
+  color: black;
+`;
+
+export const ProfileImage = styled.img`
+  height: 100px;
+  width: 100px;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-bottom: 15px;
+`;
