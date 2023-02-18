8030e737aefe8a8c48f3be4f01c33fbf9df96f66
Igor Gomes <iguinhogms@gmail.com>
2023-02-18 19:25:00 -0300

    test

--- src/pages/components/Carrossel/index.jsx
+++ src/pages/components/Carrossel/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import './carrossel.css'; // requires a loader
 import { useNavigate } from 'react-router-dom';
 
 import { Container, Slider, Image, Title } from './styles';
